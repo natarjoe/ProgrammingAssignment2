22cb658bb6c4f3ea74c430bb378eb4fd1892e53c
Joseph Natarajan <natarjoe@hotmail.com>
2016-06-11 21:46:46 -0400

    Lexical Scoping Assignment
    Lexical Scoping Assignment submitted by Joseph Natarajan

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the 2 functions below is to take advantage of the different environments available
+## in R to cache results obtained from lengthy  calculations so we dont have to repeat them over again 
+## 
 
-## Write a short comment describing this function
+## The makeCacheMatrix returns a list of four functions that set and get the matrix which is passed
+## as a parameter and set and get inverse of the matrix that has been passed to the function
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  mi <-NULL
+  set <-function(y) {
+    x<<-y
+    mi <<-NULL
+    
+  }
+  
+  get <-function (){
+    x
+    }
+  
+  setInverse <- function(Inv) {
+    mi <<- Inv                     ## set the value of inverse of martrix in parent environment of this funcion
+    }
+  getInverse <-function(){
+    mi
+  }
+  
+  list(set=set, get=get,setInverse=setInverse,getInverse=getInverse)
 }
+  
+  
 
 
-## Write a short comment describing this function
+
+
+## This function returns the value of the inverse matrix. It first  checks the cache and if the 
+## inverse is already calculated it returns it . Else it calculates the inverse and sets the value in 
+## the parent envitronment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  mi<-x$getInverse()
+  if(!is.null(mi)){
+    
+    message("getting cached data")
+    return (mi)
+    
+  }
+  
+  mat<-x$get()
+  mi<-solve(mat,...) ## calculate inverse
+  x$setInverse(mi)
+  mi
+  
 }
